998f67e6064b2d450a68c1f7d017f8d72d3988e9
wanloparu <wanlop.aru@rmutr.ac.th>
2021-12-13 04:06:18 +0700

    ep 1

--- app/src/main/java/com/example/mykotlin/HomeActivity.kt
+++ app/src/main/java/com/example/mykotlin/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mykotlin
 
+import android.icu.lang.UCharacter
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -8,11 +9,16 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.viewpager2.widget.ViewPager2
 import com.example.mykotlin.ui.main.SectionsPagerAdapter
 import com.example.mykotlin.databinding.ActivityHomeBinding
+import com.example.mykotlin.databinding.CustomTabMenuBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var sectionsPagerAdapter: SectionsPagerAdapter
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,16 +27,52 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = binding.viewPager
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = binding.tabs
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = binding.fab
+        sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager , lifecycle)
 
-        fab.setOnClickListener { view ->
+        setupViewPager()
+        setupWidget()
+        setupTabs()
+
+
+
+
+
+        }
+
+    private fun setupTabs() {
+        TabLayoutMediator(binding.tabs,binding.viewPager, TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+
+            val binding = CustomTabMenuBinding.inflate(layoutInflater)
+            binding.iconTab.setImageResource(sectionsPagerAdapter.tabIcon[position])
+            binding.textTab.text=sectionsPagerAdapter.tabText[position]
+
+            tab.customView = binding.root
+        }).attach()
+    }
+
+    private fun setupWidget() {
+
+        binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+    }
+}
+
+    private fun setupViewPager() {
+        binding.viewPager.apply {
+            adapter = sectionsPagerAdapter
+        } .also {
+            it.setPageTransformer(HorizontalFlipTransformation())
+            it.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    if (position == 0){
+                        binding.fab.visibility = View.INVISIBLE
+                    }else{
+                        binding.fab.visibility = View.VISIBLE
+                    }
+                }
+            })
         }
     }
 }
--- app/src/main/java/com/example/mykotlin/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/mykotlin/ui/main/SectionsPagerAdapter.kt
@@ -4,32 +4,18 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.mykotlin.R
 
-private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
-)
 
-/**
- * A [FragmentPagerAdapter] that returns a fragment corresponding to
- * one of the sections/tabs/pages.
- */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
-    FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager, lifecycle: Lifecycle) :FragmentStateAdapter  (fm, lifecycle) {
 
-    override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
+    val tabIcon: Array<Int> = arrayOf(R.drawable.ic_product, R.drawable.ic_stock)
+    val tabText: Array<String> = arrayOf(*context.resources.getStringArray(R.array.tab_title))
+
+    override fun getItemCount() = tabIcon.size
+    override fun createFragment(position: Int): Fragment {
         return PlaceholderFragment.newInstance(position + 1)
-    }
-
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
-    }
-
-    override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
     }
 }
